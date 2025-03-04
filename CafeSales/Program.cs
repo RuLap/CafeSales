3198956c1673c830db6de6c98c4b4157baaaf798
Igor Meshkov <48246916+RuLap@users.noreply.github.com>
2025-03-04 23:34:23 +0500

    Added docker

--- CafeSales/Program.cs
+++ CafeSales/Program.cs
@@ -4,6 +4,14 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+var host = Environment.GetEnvironmentVariable("POSTGRES_HOST");
+var db = Environment.GetEnvironmentVariable("POSTGRES_DB");
+var user = Environment.GetEnvironmentVariable("POSTGRES_USER");
+var password = Environment.GetEnvironmentVariable("POSTGRES_PASSWORD");
+
+var connectionString = $"Host={host};Database={db};Username={user};Password={password}";
+
+
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
